c3adb9a15ea8ea7c5d133964ea36cfb145c3f551
assembly <26307123-diaviekone13@users.noreply.replit.com>
2024-12-29 10:50:26 +0000

    Automatic commit before Assistant edits

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,23 +1,21 @@
-#include <iostream>
 #include <cmath>
-#include <string>
+#include <iostream>
 #include <sstream>
+#include <string>
 using namespace std;
 typedef unsigned long long ull;
 ull range;
-double result[4] = {0,0,0,0};
-ull initializer[4] = {0,0,0,0};
+double result[4] = {0, 0, 0, 0};
+ull initializer[4] = {0, 0, 0, 0};
 string range_string;
-void stress1(ull range){
-  for(; initializer[0] <= range; initializer[0]++){
-    result[0] = cos(sin(tan(initializer[0])))/sin(initializer[0]);
+volatile void stress1(ull range) {
+  for (; initializer[0] <= range; initializer[0]++) {
+    result[0] = cos(sin(tan(initializer[0]))) / sin(initializer[0]);
     return;
   }
 }
-int main(){
-  cout << "Please enter range: ";
-  cin >> range_string;
-  range = stod(range_string);
+int main() {
+  range = 1e7;
   stress1(range);
   cout << "[PROGRAMED FINISHED]\n";
   return 0;
