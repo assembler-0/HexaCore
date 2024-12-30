50b68e8fc1fe26df2fa01d982d2c4f555d8299df
assembler-0 <diaviekone13@gmail.com>
2024-12-30 07:38:05 +0000

    UPDATE

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -7,6 +7,21 @@
 #include <string>
 typedef double db;
 using namespace std;
+string file_display_menu ("extras/menu.txt");
+void displayFileContent(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << filename << std::endl;
+        return;
+    }
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+    file.close();
+}
 int main(int argc, char *argv[]) {
-  
+  cout << "HexaCore " << version << endl;
+  displayFileContent(file_display_menu);
+  return 0;
 }
