fed22b6fedc273cf7443d149bc1d3b0a03956f54
assembler-0 <diaviekone13@gmail.com>
2024-12-31 10:54:29 +0000

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "../include/CLI11.hpp"
 #include "../include/exprtk.hpp"
+#include "../include/termcolor.hpp"
 #include <cmath>
 #include <fstream>
 #include <iostream>
@@ -7,24 +8,8 @@
 #include <string>
 typedef double db;
 using namespace std;
-int main() {
-    // Define a string to hold the mathematical expression
-    string expression = "2 * (3 + 5)";
-
-    exprtk::parser<double> parser;
-
-    // Define the expression object
-    exprtk::expression<double> expr;
-
-    // Compile the expression from the string
-    parser.compile(expression, expr);
-
-    // Evaluate the expression
-    double result = expr.value();
-
-    // Output the result
-    cout << "Result: " << result << endl;
-
+int main(int argc, char** argv) {
+    cout << termcolor::red << "hello world" << termcolor::reset << endl;
     return 0;
 }
  
