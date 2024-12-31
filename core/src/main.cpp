4d998a61211d595dd924dbb70870ee66e9b20215
assembler-0 <diaviekone13@gmail.com>
2024-12-31 00:46:21 +0000

    UPDATE

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,7 +1,6 @@
 #include "../headers/CLI11.hpp"
 #include "../headers/exprtk.hpp"
 #include "../headers/tabulate.hpp"
-#include "../headers/table.hpp"
 #include <cmath>
 #include <fstream>
 #include <iostream>
@@ -9,23 +8,13 @@
 #include <string>
 typedef double db;
 using namespace std;
-using namespace tabulate;
-
 int main() {
-    Table table;
+    tabulate::Table table;
 
-    // Add header
     table.add_row({"Name", "Age", "Occupation"});
-
-    // Add rows
     table.add_row({"Alice", "25", "Engineer"});
     table.add_row({"Bob", "30", "Doctor"});
     table.add_row({"Charlie", "35", "Teacher"});
-
-    // Format table
-    table[0].format()
-        .font_style({FontStyle::bold})
-        .font_align(FontAlign::center);
 
     std::cout << table << std::endl;
     return 0;
